00fbaddbe957204c52eb02a1c51e73994d1a1dac
Yesser <benselmayasser1@gmail.com>
2023-06-09 02:37:36 +0200

    added Number selection

--- MiniProjet/lib/B.dart
+++ MiniProjet/lib/B.dart
@@ -8,8 +8,8 @@ class B extends StatefulWidget {
 }
 
 class _BState extends State<B> {
-
   final _formKey = GlobalKey<FormState>();
+  String? dropdownValue;
 
   @override
   Widget build(BuildContext context) {
@@ -21,41 +21,80 @@ class _BState extends State<B> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-
             children: [
+              DropdownButtonFormField<String>(
+                items: const [
+                  DropdownMenuItem(value: "n 1", child: Text("1")),
+                  DropdownMenuItem(value: "n 2", child: Text("2")),
+                  DropdownMenuItem(value: "n 3", child: Text("3")),
+                ],
+                value: dropdownValue,
+                onChanged: (value) {
+                  setState(() {
+                    dropdownValue = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Select an option",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 15.0),
               TextFormField(
                 decoration: InputDecoration(
-                  labelText: "Enter smth",
+                  labelText: "Enter something",
                   hintText: "idk man",
                   border: OutlineInputBorder(),
                 ),
-                validator: (value){
-                  if (value == null || value.isEmpty){
-                    return "Remplis le champ";
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please fill in the field";
                   }
                   return null;
                 },
               ),
-
-              SizedBox(
-                height: 15.0,
+              SizedBox(height: 15.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Enter something",
+                  hintText: "idk man",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please fill in the field";
+                  }
+                  return null;
+                },
               ),
-
+              SizedBox(height: 15.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Enter something",
+                  hintText: "idk man",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please fill in the field";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15.0),
               SizedBox(
                 width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(onPressed: (){
-                    if (_formKey.currentState!.validate()){
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
                       print("Hello world");
                     }
                     FocusScope.of(context).requestFocus(FocusNode());
-                  }, child: Text("Enregistrer"),
-                  ),
+                  },
+                  child: Text("Save"),
+                ),
               ),
-
-
-
-
             ],
           ),
         ),
