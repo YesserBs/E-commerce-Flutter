be89070703914eac2d75bc18f97cd880ee872d17
Yesser <benselmayasser1@gmail.com>
2023-06-10 17:26:59 +0200

    Communication entre classes

--- MiniProjet/lib/main.dart
+++ MiniProjet/lib/main.dart
@@ -1,48 +1,66 @@
 import 'package:flutter/material.dart';
-import 'A.dart';
-import 'B.dart';
-import 'C.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  int _currentIndex = 0;
-  int isC = 0;
-
-  affecterIndex(int index)
-  {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: [A(), B()][_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) => affecterIndex(index),
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "accueil",
+      title: 'Page Navigation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Page1(),
+    );
+  }
+}
+
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  String submittedText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Submitted Text:', // Display a label for the submitted text
+              style: TextStyle(fontSize: 20),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.euro),
-              label: "paiement",
-            )
+            SizedBox(height: 10),
+            Text(
+              submittedText, // Display the submitted text
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              child: Text('Go to Page 2'), // Button to navigate to Page 2
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Page2()),
+                ).then((value) {
+                  // When returning from Page 2, update the submitted text
+                  if (value != null) {
+                    setState(() {
+                      submittedText = value;
+                    });
+                  }
+                });
+              },
+            ),
           ],
         ),
       ),
@@ -50,8 +68,46 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+class Page2 extends StatefulWidget {
+  @override
+  _Page2State createState() => _Page2State();
+}
 
+class _Page2State extends State<Page2> {
+  final _textController = TextEditingController();
 
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _textController,
+              decoration: InputDecoration(labelText: 'Enter Text'), // Text input field
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              child: Text('Submit'), // Button to submit the entered text
+              onPressed: () {
+                String submittedText = _textController.text;
+                Navigator.pop(context, submittedText); // Return the text to Page 1
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
