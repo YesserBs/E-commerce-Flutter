6fae6b93a1ebff8f88b5382c6162c4e1c02e9e64
Yesser <benselmayasser1@gmail.com>
2023-06-10 23:37:12 +0200

    Navigating with a Card

--- MiniProjet/lib/main.dart
+++ MiniProjet/lib/main.dart
@@ -17,95 +17,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Page1 extends StatefulWidget {
-  @override
-  _Page1State createState() => _Page1State();
-}
-
-class _Page1State extends State<Page1> {
-  String submittedText = '';
-
+class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Page 1'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Submitted Text:', // Display a label for the submitted text
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Page2()),
+          );
+        },
+        child: Card(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Tap this card to go to Page 2',
               style: TextStyle(fontSize: 20),
             ),
-            SizedBox(height: 10),
-            Text(
-              submittedText, // Display the submitted text
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              child: Text('Go to Page 2'), // Button to navigate to Page 2
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Page2()),
-                ).then((value) {
-                  // When returning from Page 2, update the submitted text
-                  if (value != null) {
-                    setState(() {
-                      submittedText = value;
-                    });
-                  }
-                });
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
 
-class Page2 extends StatefulWidget {
-  @override
-  _Page2State createState() => _Page2State();
-}
-
-class _Page2State extends State<Page2> {
-  final _textController = TextEditingController();
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
-  }
-
+class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Page 2'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: _textController,
-              decoration: InputDecoration(labelText: 'Enter Text'), // Text input field
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              child: Text('Submit'), // Button to submit the entered text
-              onPressed: () {
-                String submittedText = _textController.text;
-                Navigator.pop(context, submittedText); // Return the text to Page 1
-              },
-            ),
-          ],
+      body: Center(
+        child: Text(
+          'Page 2',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
     );
