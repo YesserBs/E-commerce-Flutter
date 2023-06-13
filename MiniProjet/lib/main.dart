077fa510ae7ad74c905fcfa229c26b2c088016ec
Yesser <benselmayasser1@gmail.com>
2023-06-13 22:47:44 +0200

    Exemple d'usage de GetX

--- MiniProjet/lib/main.dart
+++ MiniProjet/lib/main.dart
@@ -1,98 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class CounterController extends GetxController {
+  var count = 0.obs; // Use ".obs" to make it observable
+
+  void increment() {
+    count.value++; // Access the value using ".value"
+  }
+}
+
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Card Index',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Page1(),
-    );
-  }
-}
-
-class Page1 extends StatefulWidget {
-  @override
-  _Page1State createState() => _Page1State();
-}
-
-class _Page1State extends State<Page1> {
-  List<int> cardIndices = [];
+  final CounterController _counterController = Get.put(CounterController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page 1'),
-      ),
-      body: ListView.builder(
-        itemCount: cardIndices.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Page2(cardIndices[index]),
-                ),
-              );
-            },
-            child: CardItem(index + 1),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            cardIndices.add(cardIndices.length + 1);
-          });
-        },
-      ),
-    );
-  }
-}
-
-class Page2 extends StatelessWidget {
-  final int index;
-
-  Page2(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page 2'),
-      ),
-      body: Center(
-        child: Text(
-          'Index: $index',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return GetMaterialApp(
+      title: 'Get Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Get Example'),
         ),
-      ),
-    );
-  }
-}
-
-class CardItem extends StatelessWidget {
-  final int index;
-
-  CardItem(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'Card $index',
-          style: TextStyle(fontSize: 20),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(() => Text(
+                'Count: ${_counterController.count.value}', // Access the value using ".value"
+                style: TextStyle(fontSize: 24),
+              )),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => _counterController.increment(),
+                child: Text('Increment'),
+              ),
+            ],
+          ),
         ),
       ),
     );
