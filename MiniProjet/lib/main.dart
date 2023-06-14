fd70ab7e8813b876be9924a0727b7d1e741b4742
Yesser <benselmayasser1@gmail.com>
2023-06-14 11:42:46 +0200

    App style calculatrice: pratique de GetX

--- MiniProjet/lib/main.dart
+++ MiniProjet/lib/main.dart
@@ -5,38 +5,71 @@ void main() {
   runApp(MyApp());
 }
 
-class CounterController extends GetxController {
-  var count = 0.obs; // Use ".obs" to make it observable
+class TextController extends GetxController {
+  // Create an observable string using RxString
+  var message = ''.obs;
+  var xInt = 0.obs;
+  var xCh = ''.obs;
+  var somme = 0.obs;
 
-  void increment() {
-    count.value++; // Access the value using ".value"
+  // Function to update the message
+  void updateMessage(String newMessage) {
+    message.value =  message.value + newMessage;
+    xCh.value = xCh.value + newMessage;
+  }
+  void updateXandSomme() {
+    if (xCh.value.length > 1) {
+      String result = xCh.substring(0, xCh.value.length - 1);
+      print(result);
+
+      //xInt.value = int.tryParse(result.value) ?? 0;
+      int? number = int.tryParse(result);
+      somme.value = somme.value + number!;
+      xCh.value = '';
+      print(somme);
+    }
   }
 }
 
 class MyApp extends StatelessWidget {
-  final CounterController _counterController = Get.put(CounterController());
+  final TextController textController = Get.put(TextController());
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Get Example',
+      title: 'GetX String Observable',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Get Example'),
+          title: Text('GetX String Observable'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Obx(() => Text(
-                'Count: ${_counterController.count.value}', // Access the value using ".value"
+                textController.message.value,
                 style: TextStyle(fontSize: 24),
               )),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () => _counterController.increment(),
-                child: Text('Increment'),
-              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        textController.updateMessage('5');
+                      },
+                      child: Text('5'),
+                    ),
+                    SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        textController.updateMessage('+');
+                        textController.updateXandSomme();
+                      },
+                      child: Text('+'),
+                    ),
+                    ],
+              )
             ],
           ),
         ),
