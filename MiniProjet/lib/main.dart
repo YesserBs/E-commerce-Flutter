3ec9ba6b16949f83137aef47b63d5d9558748b6e
Yesser <benselmayasser1@gmail.com>
2023-06-14 12:54:07 +0200

    Calculatrice (affectation selment et 'Clear' de l'ecran manuel)

--- MiniProjet/lib/main.dart
+++ MiniProjet/lib/main.dart
@@ -14,10 +14,17 @@ class TextController extends GetxController {
 
   // Function to update the message
   void updateMessage(String newMessage) {
-    message.value =  message.value + newMessage;
-    xCh.value = xCh.value + newMessage;
+    if (newMessage == ''){
+      message.value = '';
+      xCh.value = '';
+      somme.value = 0;
+    }
+    else{
+      message.value =  message.value + newMessage;
+      xCh.value = xCh.value + newMessage;
+    }
   }
-  void updateXandSomme() {
+  void updateX(String x) {
     if (xCh.value.length > 1) {
       String result = xCh.substring(0, xCh.value.length - 1);
       print(result);
@@ -26,8 +33,13 @@ class TextController extends GetxController {
       int? number = int.tryParse(result);
       somme.value = somme.value + number!;
       xCh.value = '';
-      print(somme);
+      //print(somme);
     }
+    if (x == "=")
+      {
+        print(somme);
+        updateMessage(somme.value.toString());
+      }
   }
 }
 
@@ -51,24 +63,107 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(fontSize: 24),
               )),
               Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 16),
-                    ElevatedButton(
-                      onPressed: () {
-                        textController.updateMessage('5');
-                      },
-                      child: Text('5'),
-                    ),
-                    SizedBox(width: 16),
-                    ElevatedButton(
-                      onPressed: () {
-                        textController.updateMessage('+');
-                        textController.updateXandSomme();
-                      },
-                      child: Text('+'),
-                    ),
-                    ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('1');
+                    },
+                    child: Text('1'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('2');
+                    },
+                    child: Text('2'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('3');
+                    },
+                    child: Text('3'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red),),
+                    onPressed: () {
+                      textController.updateMessage('');
+                      textController.updateX('C');
+                    },
+                    child: Text('C'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('4');
+                    },
+                    child: Text('4'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('5');
+                    },
+                    child: Text('5'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('6');
+                    },
+                    child: Text('6'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('+');
+                      textController.updateX('+');
+                    },
+                    child: Text('+'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('7');
+                    },
+                    child: Text('7'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('8');
+                    },
+                    child: Text('8'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('9');
+                    },
+                    child: Text('9'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      textController.updateMessage('=');
+                      textController.updateX('=');
+                    },
+                    child: Text('='),
+                  ),
+                ],
               )
             ],
           ),
