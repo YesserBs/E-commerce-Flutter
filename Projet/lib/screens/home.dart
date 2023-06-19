a133b45db181c7ffcdbf2336395411645632beb1
Yesser <benselmayasser1@gmail.com>
2023-06-19 12:04:45 +0200

    +Items +NavBar

--- Projet/lib/screens/home.dart
+++ Projet/lib/screens/home.dart
@@ -1,14 +1,100 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final List<String> items = <String>["IPhone A", "IPhone B", "IPhone C", "IPhone D", "IPhone E", "IPhone A", "IPhone B", "IPhone C", "IPhone D", "IPhone E","IPhone A", "IPhone B", "IPhone C", "IPhone D", "IPhone E"];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Hello world"),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 20.0),
+          child: Column(
+            children: [
+              _SearchFormField(),
+              _Category(),
+              _ListItems(),
+              _BottomNav()
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+Widget _ListItems() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 100,
+            child: Card(
+              child: ListTile(
+                leading: Image.asset('assets/images/IPhone.jpg'),
+                title: Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+                    child: Text(items[index])),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Brand/ Seller'),
+                    SizedBox(height: 2.0,),
+                    Text('1000 dts'),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+Widget _Category() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(13.0, 12.0, 0.0, 12.0),
+      child: Row(
+        children: [
+          Icon(FontAwesomeIcons.chevronDown, size: 15.0,),
+          SizedBox(width: 5.0,),
+          Text("all"),
+        ],
+      ),
+    );
+}
+}
+
+Widget _SearchFormField() {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.grey[200],
+      borderRadius: BorderRadius.circular(40),
+    ),
+    child: TextFormField(
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        prefixIcon: Icon(Icons.search, color: Colors.black,),
+      ),
+    ),
+  );
+}
+
+Widget _BottomNav() {
+  return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          label: "Explore",
+            icon: Icon(Icons.home)
+        ),
+        BottomNavigationBarItem(
+            label: "Explore",
+            icon: Icon(Icons.home)
+        ),
+      ],
+    currentIndex: 0,
+    onTap: (index){},
+  );
 }
