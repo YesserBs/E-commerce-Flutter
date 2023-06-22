597e96a97779dcc910343c044217bdce863eb9ad
Yesser <benselmayasser1@gmail.com>
2023-06-22 16:12:53 +0200

    New UI to practice Getx

--- Projet/lib/pages/account/account_page.dart
+++ Projet/lib/pages/account/account_page.dart
@@ -1,63 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
 
-import '../../const/MyWidget.dart';
-import 'account_controller.dart';
-
-class AccountPage extends GetView<AccountController> {
+class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                SizedBox(height: 55.0,),
-                Center(
-                  child: Container(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: CircleAvatar(
-                        radius: 70,
-                        backgroundImage: AssetImage('assets/images/profile.jpg'),
-                      ),
-                    ),
-                  ),
-                ),
-            SizedBox(height: 20.0,),
-            Text("Username here",
-            style: TextStyle(fontSize: 25.0, fontFamily: 'OstrichSans', color: Colors.grey[900]),),
-            SizedBox(height: 50.0,),
-                CustomWidget(
-                  //image: "assets/images/flutterImage.png",
-                  text: 'Hello, world!',
-                  //indent: true,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-
-
-              ],
-            ),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red[200], // Set button color to red
           ),
+          onPressed: () {
+            // Handle button press
+          },
+          icon: Icon(Icons.add,color: Colors.black),
+          label: Text('Add', style: TextStyle(color: Colors.black),),
         ),
       ),
     );
   }
 }
+
+
+
+
+
--- Projet/lib/pages/home/home_page.dart
+++ Projet/lib/pages/home/home_page.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:myproj/pages/details/details_page.dart';
-import 'package:myproj/pages/home/home_controller.dart';
 
 
 class HomePage extends StatelessWidget {
-  final List<String> items = <String>["IPhone A", "IPhone B", "IPhone C"];
-  final List<String> pics = <String>[
-    'assets/images/IPhone.jpg',
-    'assets/images/IPH5.webp',
-    'assets/images/IPH4.webp',];
+  final List<String> items = <String>["Hello world!"];
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +12,7 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: [
-            _SearchFormField(),
-            _Category(),
             _ListItems(),
-            Divider()
           ],
         ),
         //bottomNavigationBar: _BottomNav(),
@@ -37,26 +28,21 @@ class HomePage extends StatelessWidget {
           return Container(
             height: 100,
             child: Card(
+              color: Colors.red[200],
               child: ListTile(
-                onTap: (){
+                onTap: () {
                   Get.to(DetailsPage());
                 },
-                leading: Image.asset(pics[index]),
-                title: Padding(
-                    padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
-                    child: Text(items[index],
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontFamily: 'OstrichSans',
-                          color: Colors.grey[900]
-                      ),)),
+                title: Center(
+                  child: Text(items[index],
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'OstrichSans',
+                        color: Colors.grey[900]
+                    ),),
+                ),
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Brand/ Seller',),
-                    SizedBox(height: 2.0,),
-                    Text('1000 dts'),
-                  ],
                 ),
               ),
             ),
@@ -65,33 +51,5 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-
-  Widget _Category() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(13.0, 12.0, 0.0, 12.0),
-      child: Row(
-        children: [
-          Icon(FontAwesomeIcons.chevronDown, size: 15.0,),
-          SizedBox(width: 5.0,),
-          Text("all"),
-        ],
-      ),
-    );
-  }
 }
 
-Widget _SearchFormField() {
-  return Container(
-    margin: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0.0),
-    decoration: BoxDecoration(
-      color: Colors.grey[200],
-      borderRadius: BorderRadius.circular(40),
-    ),
-    child: TextFormField(
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        prefixIcon: Icon(Icons.search, color: Colors.black,),
-      ),
-    ),
-  );
-}
