40bc71c1b1145a7163170637acdbd1a6ad021b32
Yesser <benselmayasser1@gmail.com>
2023-06-26 11:35:39 +0200

    SearchBar example

--- Projet/lib/main.dart
+++ Projet/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'routes/app_pages.dart';
-import 'routes/app_routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +9,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialRoute: AppRoutes.DASHBOARD,
-      getPages: AppPages.list,
-      debugShowCheckedModeBanner: false,
+      title: 'Search List Example',
+      home: MyHomePage(),
     );
   }
 }
+
+class MyHomePage extends StatelessWidget {
+  final MyController _controller = Get.put(MyController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search List'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              onChanged: _controller.filterList,
+              decoration: InputDecoration(
+                labelText: 'Search',
+              ),
+            ),
+          ),
+          Expanded(
+            child: Obx(() => ListView.builder(
+              itemCount: _controller.filteredItems.length,
+              itemBuilder: (context, index) {
+                final item = _controller.filteredItems[index];
+                return Card(
+                  child: ListTile(
+                    title: Text(item),
+                  ),
+                );
+              },
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyController extends GetxController {
+  final items = [
+    'apple',
+    'banana',
+    'orange',
+    'grape',
+    'watermelon',
+    'kiwi',
+  ].obs;
+
+  final filteredItems = <String>[].obs;
+
+  MyController() {
+    filteredItems.assignAll(items);
+  }
+
+  void filterList(String value) {
+    if (value.isEmpty) {
+      filteredItems.assignAll(items);
+    } else {
+      filteredItems.assignAll(items.where((item) => item.contains(value)));
+    }
+  }
+}
+
