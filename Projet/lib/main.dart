0d4cb74bc32a15d93f4bb6664b2797f013b6fe7f
Yesser <benselmayasser1@gmail.com>
2023-07-08 00:54:44 +0200

    Simple working example

--- Projet/lib/main.dart
+++ Projet/lib/main.dart
@@ -1,30 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:myproj/pages/dashboard/dashboard_binding.dart';
-import 'routes/app_pages.dart';
-import 'routes/app_routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:get/get.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+  await Firebase.initializeApp();
+  runApp(GetMaterialApp(home: LoginForm()));
 }
 
-class MyApp extends StatelessWidget {
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _errorMessage = '';
+
+  Future<void> _login() async {
+    try {
+      if (_formKey.currentState!.validate()) {
+        final String email = _emailController.text.trim();
+        final String password = _passwordController.text.trim();
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        if (userCredential.user != null) {
+          Get.offAll(HomeScreen()); // Navigate to HomeScreen
+        }
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = e.message!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: AppRoutes.AUTH,
-      //initialBinding: DashboardBinding(),
-      theme: ThemeData(colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.grey[900]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
       ),
-      getPages: AppPages.list,
-      debugShowCheckedModeBanner: false,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _login,
+                child: Text('Login'),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                _errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
     );
   }
 }
