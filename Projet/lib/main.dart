08151142fd61199b7eebb71e79070280519a3903
Yesser <benselmayasser1@gmail.com>
2023-07-08 18:21:32 +0200

    Simple auth example with firebase

--- Projet/lib/main.dart
+++ Projet/lib/main.dart
@@ -9,38 +9,8 @@ void main() async {
   runApp(GetMaterialApp(home: LoginForm()));
 }
 
-
-class LoginForm extends StatefulWidget {
-  @override
-  _LoginFormState createState() => _LoginFormState();
-}
-
-class _LoginFormState extends State<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  String _errorMessage = '';
-
-  Future<void> _login() async {
-    try {
-      if (_formKey.currentState!.validate()) {
-        final String email = _emailController.text.trim();
-        final String password = _passwordController.text.trim();
-        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-        if (userCredential.user != null) {
-          Get.offAll(HomeScreen()); // Navigate to HomeScreen
-        }
-      }
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _errorMessage = e.message!;
-      });
-    }
-  }
+class LoginForm extends StatelessWidget {
+  final _controller = Get.put(LoginController());
 
   @override
   Widget build(BuildContext context) {
@@ -51,11 +21,11 @@ class _LoginFormState extends State<LoginForm> {
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Form(
-          key: _formKey,
+          key: _controller.formKey,
           child: Column(
             children: [
               TextFormField(
-                controller: _emailController,
+                controller: _controller.emailController,
                 decoration: InputDecoration(labelText: 'Email'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -65,7 +35,7 @@ class _LoginFormState extends State<LoginForm> {
                 },
               ),
               TextFormField(
-                controller: _passwordController,
+                controller: _controller.passwordController,
                 decoration: InputDecoration(labelText: 'Password'),
                 obscureText: true,
                 validator: (value) {
@@ -77,13 +47,22 @@ class _LoginFormState extends State<LoginForm> {
               ),
               SizedBox(height: 16.0),
               ElevatedButton(
-                onPressed: _login,
+                onPressed: _controller.login,
                 child: Text('Login'),
               ),
               SizedBox(height: 16.0),
-              Text(
-                _errorMessage,
-                style: TextStyle(color: Colors.red),
+              TextButton(
+                onPressed: () {
+                  Get.to(SignupForm());
+                },
+                child: Text('Create an account'),
+              ),
+              SizedBox(height: 16.0),
+              Obx(
+                    () => Text(
+                  _controller.errorMessage.value,
+                  style: TextStyle(color: Colors.red),
+                ),
               ),
             ],
           ),
@@ -93,7 +72,13 @@ class _LoginFormState extends State<LoginForm> {
   }
 }
 
+class HomeController extends GetxController {
+  // Controller logic for HomeScreen can be added here
+}
+
 class HomeScreen extends StatelessWidget {
+  final _controller = Get.put(HomeController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,5 +89,125 @@ class HomeScreen extends StatelessWidget {
         child: Text('Welcome to the Home Screen!'),
       ),
     );
+  }
+}
+
+class LoginController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  var errorMessage = ''.obs;
+
+  void login() async {
+    try {
+      if (formKey.currentState!.validate()) {
+        final String email = emailController.text.trim();
+        final String password = passwordController.text.trim();
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        if (userCredential.user != null) {
+          Get.offAll(HomeScreen()); // Navigate to HomeScreen
+        }
+      }
+    } on FirebaseAuthException catch (e) {
+      errorMessage.value = e.message!;
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
+
+class SignupForm extends StatelessWidget {
+  final _controller = Get.put(SignupController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _controller.formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controller.emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _controller.passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _controller.signup,
+                child: Text('Sign Up'),
+              ),
+              SizedBox(height: 16.0),
+              Obx(
+                    () => Text(
+                  _controller.errorMessage.value,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignupController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  var errorMessage = ''.obs;
+
+  void signup() async {
+    try {
+      if (formKey.currentState!.validate()) {
+        final String email = emailController.text.trim();
+        final String password = passwordController.text.trim();
+        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        if (userCredential.user != null) {
+          Get.offAll(HomeScreen()); // Navigate to HomeScreen
+        }
+      }
+    } on FirebaseAuthException catch (e) {
+      errorMessage.value = e.message!;
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 }
